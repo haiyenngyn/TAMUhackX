592ac6af750c14dc3b5e4ee284e625c76e5c1a9b
RodrigoOrozco <rodrigoorozco025@gmail.com>
2024-01-27 12:53:30 -0600

    Empty rep

---
+++ TAMU-HACK-X/TAMU_HACK_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  TAMU_HACK_XApp.swift
+//  TAMU-HACK-X
+//
+//  Created by rodrigo Orozco  on 1/27/24.
+//
+
+import SwiftUI
+
+@main
+struct TAMU_HACK_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
