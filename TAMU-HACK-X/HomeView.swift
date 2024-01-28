3c79b28f6c2068ff2135e9f56f10d3bfaf605e7c
haiyennguyen <haiyenngyn159@gmail.com>
2024-01-28 11:26:38 -0600

    changed some headers

--- TAMU-HACK-X/HomeView.swift
+++ TAMU-HACK-X/HomeView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationStack {
-            Text("Top 10 Destinations of the Week")
+            Text("Top Destinations of the Week")
                 .fontWeight(.bold)
                 .font(.title2)
             
